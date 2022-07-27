8ea7fb864e727a863e088fe3e1c30297a01cd017
Wesley Barros <wesley.barros@tech4h.com.br>
2022-07-26 21:28:57 -0300

    Criação do projeto

---
+++ server/index.js
@@ -0,0 +1,70 @@
+ const express = require("express");
+ const app = express();
+const mysql = require("mysql");
+const cors = require('cors');
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+
+const db = mysql.createPool({
+ host: "localhost",
+ user: "root",
+ password: "",
+ databse: "banco",
+});
+
+app.use(express.json());
+app.use(cors());
+
+app.post("/register", (req, res) => {
+ const email = req.body.email;
+ const password = req.body.password;
+
+ db.query("SELECT * FROM banco.usuarios WHERE email = ?", [email], (err, result) => {
+  if(err){
+   res.send(err);
+  }
+  if (result.length == 0){
+   bcrypt.hash(password, saltRounds, (erro, hash) => {
+    db.query("INSERT INTO banco.usuarios (email, password) VALUES (?, ?)", [email, hash], (err, response) => {
+     if (err) {
+      res.send(err)
+     }
+ 
+     res.send({msg: "Cadastrado com sucesso"});
+    });
+   })
+
+  }else{
+   res.send({ msg: "Usuário já cadastrado"});
+  }
+ });
+});
+
+app.post("/login", (req, res) => {
+ const email = req.body.email;
+ const password = req.body.password;
+
+ db.query("SELECT * FROM banco.usuarios WHERE email = ?", [email],
+ (err, result) => {
+  if(err){
+   req.send(err);
+  }
+  if(result.length > 0){
+   bcrypt.compare(password, result[0].password, (erro, result) => {
+    if(result){
+     res.send("Usuário logado com sucesso")
+    }else {
+     res.send("Senha está incorreta")
+    }
+   });
+  
+  }else {
+   res.send({ msg: "Conta não encontrada" });
+  }
+ })
+});
+
+ app.listen(3001, () => {
+  console.log("Rodando na porta 3001");
+ });
